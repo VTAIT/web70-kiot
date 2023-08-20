d493a32ec3995c059d7a67aaca4b12d049a52b83
Lan Pham <lan.pham@dfs.de>
2023-08-20 12:37:59 +0200

    adding connection to my DB to test

--- globals/mongodb.js
+++ globals/mongodb.js
@@ -1,19 +1,18 @@
-import mongoose from 'mongoose';
-import { env } from './config.js'
-import { AccountSchema } from '../models/model_account.js';
-import { ProductSchema } from '../models/model_product.js';
+import mongoose from "mongoose";
+import { env } from "./config.js";
+import { AccountSchema } from "../models/model_account.js";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`mongodb://127.0.0.1:${env.PORT_MONGODB}/${env.DBNAME}`);
-        console.log(`Connect Successs: ${env.HOSTNAME}:${env.PORT_MONGODB}/${env.DBNAME}`)
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    // await mongoose.connect(`mongodb://127.0.0.1:${env.PORT_MONGODB}/${env.DBNAME}`);
+    // console.log(`Connect Successs: ${env.HOSTNAME}:${env.PORT_MONGODB}/${env.DBNAME}`)
+    const connection = await mongoose.connect(env.MONGO_URI);
+    console.log(`Connect to database at ${connection.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const UserModel = mongoose.model('users', AccountSchema);
-const ProductModel = mongoose.model('products', ProductSchema);
-const RegisterModel = mongoose.model('Registers', AccountSchema);
+const UserModel = mongoose.model("users", AccountSchema);
 
-export { connectDB, UserModel, ProductModel, RegisterModel };
+export { connectDB, UserModel };
