cef4215e9554ba41ccea06e014600f53461fd4b2
AnVT <58844911+VTAIT@users.noreply.github.com>
2023-08-30 09:15:17 +0700

    Update config.js

--- globals/config.js
+++ globals/config.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 const env = process.env;
 const defaultImage = "1kvFDWul0NlJiF4Pc5fCGAdMqXhWWkUPY";
 const salt = await bcrypt.genSalt(Number(env.SALT));
-const limit = 3
+const limit = 50
 
 const hashPassWord = async (password) => {
     const hash = await bcrypt.hash(password, salt);
