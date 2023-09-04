2ec25bd0a6f13255294b0eae597956ae1068ee04
Lan Pham <lan.pham@dfs.de>
2023-09-04 15:37:44 +0200

    adding api to upload avatar in profile

--- routes/account/index.js
+++ routes/account/index.js
@@ -4,6 +4,7 @@ import { jwtCheck } from "../../middlewares/jwt.js";
 import ReadRouter from "./read.js";
 import CreateRouter from "./create.js";
 import AcceptRouter from "./accept.js";
+import AvatarUploadRouter from "./avatarUpload.js";
 import { CheckRoleAccount } from "../../middlewares/check.js";
 
 const AccountRoute = Router();
@@ -13,5 +14,6 @@ AccountRoute.use("/", ReadRouter);
 AccountRoute.use("/create", CreateRouter);
 AccountRoute.use("/update", UpdateRouter);
 AccountRoute.use("/accept", AcceptRouter);
+AccountRoute.use("/upload-avatar", AvatarUploadRouter);
 
 export default AccountRoute;
